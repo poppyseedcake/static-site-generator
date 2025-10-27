5080629dcdae13a992976e7b88ab77b252805eb9
poppyseedcake <w.makowiec@gmail.com>
2025-10-27 19:19:37 +0100

    dsdas

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode, TextType
+
+def main():
+    node = TextNode("Some text", TextType.LINK, "http://google.com")
+    print(repr(node))
+
+if __name__ == "__main__":
+    main()
