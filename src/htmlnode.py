41dc0c0e24f7d134a92fc6ca1470d34a0b6932f8
poppyseedcake <w.makowiec@gmail.com>
2025-10-29 20:06:35 +0100

    htmlnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -6,11 +6,15 @@ class HTMLNode:
         self.props = props
 
     def to_html(self):
-        raise NotImplementedError
+        raise NotImplementedError("to_html method not implemented")
 
-    def props_to_html(sefl):
-        str = ""
-        for k, v in self.props:
-            str += f" {k} {v}"
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
 
-        return str
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
