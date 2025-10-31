0363a426197e3c05872150efda3dd0da8019475b
poppyseedcake <w.makowiec@gmail.com>
2025-10-31 22:42:59 +0100

    dsaDa

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from textnode import TextNode, TextType
 def main():
     node = TextNode("Some text", TextType.LINK, "http://google.com")
     print(repr(node))
-
+    #
 
 
 if __name__ == "__main__":
