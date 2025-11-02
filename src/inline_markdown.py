968ddd31ab89b2d05735814589e3a26acd225302
poppyseedcake <w.makowiec@gmail.com>
2025-11-02 23:04:34 +0100

    mdasdas

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -79,3 +79,11 @@ def split_nodes_link(old_nodes):
             new_nodes.append(TextNode(old_node_text, TextType.TEXT))
     return new_nodes
 
+def text_to_textnodes(text):
+    node = [TextNode(text, TextType.TEXT)]
+    node = split_nodes_delimiter(node, "**", TextType.BOLD)
+    node = split_nodes_delimiter(node, "_", TextType.ITALIC)
+    node = split_nodes_delimiter(node, "`", TextType.CODE)
+    node = split_nodes_image(node)
+    node = split_nodes_link(node)
+    return node
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -5,9 +5,11 @@ from inline_markdown import (
     split_nodes_delimiter,
     split_nodes_image,
     split_nodes_link,
+    text_to_textnodes,
 )
 
 from textnode import TextNode, TextType
+import textnode
 
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -207,6 +209,26 @@ class TestInlineMarkdown(unittest.TestCase):
             ],
             new_nodes,
         )
+
+    def test_text_totextnode(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        text_nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            text_nodes,
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
