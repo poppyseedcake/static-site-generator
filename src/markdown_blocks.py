a17a1ce6dc22aaa09294de3474764517fc82c390
poppyseedcake <w.makowiec@gmail.com>
2025-11-04 19:50:44 +0100

    blocks

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,4 +1,4 @@
-#def markdown_to_blocks(markdown):
+from enum import Enum
 #    blocks = markdown.split("\n\n")
 #    filtered_blocks = []
 #    for block in blocks:
@@ -14,3 +14,37 @@ def markdown_to_blocks(markdown):
     while "" in blocks:
         blocks.remove("")
     return blocks
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.ULIST
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.OLIST
+    return BlockType.PARAGRAPH
