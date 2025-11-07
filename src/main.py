f996e07f814d6a7266f3784fc7d29c8254d7aa04
poppyseedcake <w.makowiec@gmail.com>
2025-11-08 00:00:54 +0100

    new solution

--- src/main.py
+++ src/main.py
@@ -1,38 +1,47 @@
-from nt import mkdir
 import os
-from os.path import isfile
 import shutil
-from textnode import TextNode, TextType
+from copystatic import copy_files_recursive
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 def main():
-    copy_static_to_public()    
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-def copy_static_to_public():    
-    if os.path.exists("./public/"):
-        print(f"Removing ./public/")
-        shutil.rmtree("./public/")
-        print(f"Creating new folder ./public")
-        os.mkdir("./public")
-    list_static("", "")
-    return
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
-def list_static(base_path, next_path):
-    abs_path = os.path.join("./static/", base_path, next_path)    
-    rel_path = os.path.join(base_path, next_path)
-    dir = os.listdir(abs_path)
-    for element in dir:
-        element_path = os.path.join(abs_path, element)
-        if os.path.isfile(element_path):
-            public_path = os.path.join("./public", rel_path)
-            if not os.path.exists(public_path):
-                print(f"Creating public path: {public_path}")
-                os.mkdir(public_path)
-            print(f"Copy {element} from {abs_path} to {public_path}")
-            shutil.copy(element_path, public_path)            
-        else:
-            print(f"{element_path} is not a file")
-            list_static(rel_path, element)
-    return
+#    copy_static_to_public()    
+
+#def copy_static_to_public():    
+#    if os.path.exists("./public/"):
+#        print(f"Removing ./public/")
+#        shutil.rmtree("./public/")
+#        print(f"Creating new folder ./public")
+#        os.mkdir("./public")
+#    list_static("", "")
+#    return
+#
+#def list_static(base_path, next_path):
+#    abs_path = os.path.join("./static/", base_path, next_path)    
+#    rel_path = os.path.join(base_path, next_path)
+#    dir = os.listdir(abs_path)
+#    for element in dir:
+#        element_path = os.path.join(abs_path, element)
+#        if os.path.isfile(element_path):
+#            public_path = os.path.join("./public", rel_path)
+#            if not os.path.exists(public_path):
+#                print(f"Creating public path: {public_path}")
+#                os.mkdir(public_path)
+#            print(f"Copy {element} from {abs_path} to {public_path}")
+#            shutil.copy(element_path, public_path)            
+#        else:
+#            print(f"found folder: {element_path}")
+#            list_static(rel_path, element)
+#    return
 
 if __name__ == "__main__":
     main()
